242e1559814f1c5c68d588a5abe9718eb32ee229
leo4812 <leo4812@icloud.com>
2022-02-09 14:58:13 +0300

    FiltrPressure

--- include/MPX5700DP.hpp
+++ include/MPX5700DP.hpp
@@ -1,12 +1,17 @@
 #include <Arduino.h>
 #include "BaseSensor.hpp"
+#include "RingAverage.h"
 
 class MPX5700DP : public BaseSensor
 {
 
 public:
+    uint32_t PressureFILTR;
+
     MPX5700DP()
     {
+        fil = new RingAverage<uint32_t, 10>();
+
         this->Name = (char *)std::string("MPX5700DP").c_str();
         this->IsAnalog = true;
         CommandCharacteristic = new BLECharacteristic("6C8B0BBC-8096-4A20-9051-1819B5001EFD", BLERead | BLEWrite, ANALOG_COMMAND_SIZE, true);
@@ -21,10 +26,17 @@ private:
     void post_loop() {}
     void loop()
     {
+
         uint32_t value = analogRead(this->AnalogPort);
-        uint8_t buffer[6] = { 0, };
+
+        auto PressureFILTR = fil->filtered(value);
+
+        uint8_t buffer[6] = {
+            0,
+        };
         buffer[1] = this->AnalogPort == A0 ? 0x00 : 0x01;
-        memcpy(&buffer[2], &value, sizeof(value));
+        memcpy(&buffer[2], &PressureFILTR, sizeof(PressureFILTR));
         this->NotifyCharacteristic->writeValue(buffer, sizeof(buffer));
     }
+    RingAverage<uint32_t, 10> *fil;
 };
