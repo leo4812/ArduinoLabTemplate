e9d85636a10271f166f63849ce165a114ba7f7bf
leo4812 <leo4812@icloud.com>
2022-05-16 12:46:51 +0300

    NewSound

--- include/Sound.hpp
+++ include/Sound.hpp
@@ -1,12 +1,26 @@
 #include <Arduino.h>
 #include "BaseSensor.hpp"
+#include "VolAnalyzer.h"
+
+using namespace rtos;
 
 class Sound : public BaseSensor
 {
 
 public:
+    VolAnalyzer *analyzer;
+
+    volatile int inSound = 0;
+
+    bool doWork = true;
+
+    volatile uint32_t outSound = 0; // пометить как volatile, глобальная переменная для отдельного потока
+
+    Thread *runThread;
+
     Sound()
     {
+
         this->Name = (char *)std::string("Sound").c_str();
         this->IsAnalog = true;
         CommandCharacteristic = new BLECharacteristic("5A1FBAEC-8E0D-4DCC-9BC6-5FF89A5FA849", BLERead | BLEWrite, ANALOG_COMMAND_SIZE, true);
@@ -14,15 +28,43 @@ public:
     }
 
 private:
+    void execute()
+    {
+        while (doWork)
+        {
+            if (this->analyzer->tick())
+            {
+                inSound = this->analyzer->getMax();
+                outSound = map(inSound, 0, 650, 20, 110);
+            }
+        }
+    }
     void pre_loop()
     {
-        pinMode(this->AnalogPort, INPUT);
+        this->analyzer = new VolAnalyzer(this->AnalogPort);
+        this->analyzer->setVolK(20);
+        this->analyzer->setTrsh(10);
+        this->analyzer->setVolMin(10);
+        this->analyzer->setVolMax(100);
+
+        doWork = true;
+        runThread = new Thread();
+        runThread->start(callback(this, &Sound::execute));
     }
-    void post_loop() {}
+    void post_loop()
+    {
+        doWork = false;
+        if (runThread != nullptr)
+        {
+            runThread = nullptr;
+        }
+    }
     void loop()
     {
-        uint32_t value = analogRead(this->AnalogPort);
-        uint8_t buffer[6] = { 0, };
+        uint32_t value = outSound;
+        uint8_t buffer[6] = {
+            0,
+        };
         buffer[1] = this->AnalogPort == A0 ? 0x00 : 0x01;
         memcpy(&buffer[2], &value, sizeof(value));
         this->NotifyCharacteristic->writeValue(buffer, sizeof(buffer));
