71a3974f2ff23abdfdde213df4592a15fdb6250c
leo4812 <leo4812@icloud.com>
2022-06-06 10:53:53 +0300

    FixPNO3

--- include/PNO3.hpp
+++ include/PNO3.hpp
@@ -1,30 +1,38 @@
 #include <Arduino.h>
 #include "BaseSensor.hpp"
+#include <Wire.h>
+#include <Adafruit_ADS1X15.h>
 
 class PNO3 : public BaseSensor
 {
 
 public:
+    Adafruit_ADS1115 ads; // Создание объекта ads для общения через него с модулем
+
     PNO3()
     {
         this->Name = (char *)std::string("PNO3").c_str();
-        this->IsAnalog = true;
-        CommandCharacteristic = new BLECharacteristic("CF47685F-09C6-40C9-A442-A48513E7690A", BLERead | BLEWrite, ANALOG_COMMAND_SIZE, true);
-        NotifyCharacteristic = new BLECharacteristic("E5E6F644-B15F-4713-9D9E-9CEB6B9D328F", BLERead | BLENotify, 6, true);
+        CommandCharacteristic = new BLECharacteristic("CF47685F-09C6-40C9-A442-A48513E7690A", BLERead | BLEWrite, DIGITAL_COMMAND_SIZE, true);
+        NotifyCharacteristic = new BLECharacteristic("E5E6F644-B15F-4713-9D9E-9CEB6B9D328F", BLERead | BLENotify, 5, true);
     }
 
 private:
     void pre_loop()
     {
-        pinMode(this->AnalogPort, INPUT);
+        ads.setGain(GAIN_TWOTHIRDS);
+        ads.begin();
     }
     void post_loop() {}
     void loop()
     {
-        uint32_t value = analogRead(this->AnalogPort);
-        uint8_t buffer[6] = { 0, };
-        buffer[1] = this->AnalogPort == A0 ? 0x00 : 0x01;
-        memcpy(&buffer[2], &value, sizeof(value));
+        int32_t adc_01_voltage; // Переменная для хранения значения АЦП вольтметра (канал 0-1)
+
+        adc_01_voltage = ads.readADC_Differential_0_1(); // Измеряем значение АЦП на канало 0-1
+
+        uint8_t buffer[5] = {
+            0,
+        };
+        memcpy(&buffer[1], (uint8_t *)&adc_01_voltage, sizeof(adc_01_voltage));
         this->NotifyCharacteristic->writeValue(buffer, sizeof(buffer));
     }
 };
