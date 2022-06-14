8c6da34afe21b83c1928a494ee5f48b0fe91cbde
leo4812 <leo4812@icloud.com>
2022-06-14 17:04:13 +0300

    remakeMAX30102

--- include/MAX30102.hpp
+++ include/MAX30102.hpp
@@ -7,86 +7,105 @@
 
 using namespace rtos;
 
-// Thread BPMcalculateBPM;
-
 class MAX30102 : public BaseSensor
 {
 
 public:
+    MAX30105 particleSensor;
+
+    long unblockedValue;
+    long lastBeat = 0;
+    float beatsPerMinute;
+    int beatAvg;
+    // const byte RATE_SIZE = 6;    // Increase this for more averaging. 4 is good.
+    byte rates[6]; // Array of heart rates
+    byte rateSpot = 0;
+
+    volatile uint32_t sinus = 0; // Переменная синусойды
+    volatile uint32_t BPM = 0;   // Переменная усредненного количества ЧСС
+
+    Thread *runThread;
+
+    bool doWork = true;
+
     MAX30102()
     {
 
         this->Name = (char *)std::string("MAX30102").c_str();
         CommandCharacteristic = new BLECharacteristic("9abe01bf-55d3-4ae2-bc09-edb1a6c209c7", BLERead | BLEWrite, DIGITAL_COMMAND_SIZE, true);
-        NotifyCharacteristic = new BLECharacteristic("ccf0279b-f3c5-4366-8b65-5dfd2d6741f8", BLERead | BLENotify, 5, true);
+        NotifyCharacteristic = new BLECharacteristic("ccf0279b-f3c5-4366-8b65-5dfd2d6741f8", BLERead | BLENotify, 9, true);
     }
 
 private:
-    bool ErrorMAX30102 = false; //Датчик не подключен
-
-    MAX30105 *max30105; //  Создаём объект для работы с библиотекой
-    Thread *runThread;
-
-    byte rates[10];       //  Массив со значениями ЧСС
-    byte rateSpot = 0;    //  Переменная с порядковым номером значения в массиве
-    long lastBeat = 0;    //  Время последнего зафиксированного удара
-    float beatsPerMinute; //  Создаём переменную для хранения значения ЧСС
-    int beatAvg = 0;      //  Создаём переменную для хранения усреднённого значения ЧСС
-    long irValue = 0;
-
-    bool doWork = true;
-
     void execute()
     {
         while (doWork)
         {
-            irValue = max30105->getIR(); //  Считываем значение отражённого ИК-светодиода (отвечающего за пульс) и
+            auto ir = particleSensor.getIR();
+            /*
+                        if (checkForBeat(ir) == true)
+                        {
+                            long delta = millis() - lastBeat;
+                            lastBeat = millis();
+                            beatsPerMinute = 60 / (delta / 1000.0);
+                            if (beatsPerMinute < 255 && beatsPerMinute > 20)
+                            {
+                                rates[rateSpot++] = (byte)beatsPerMinute; // Store this reading in the array
+                                rateSpot %= 6;                    // Wrap variable
 
-            // Serial.println(checkForBeat(irValue));
-
-            if (checkForBeat(irValue) == true)
-            {                                           //  если пульс был зафиксирован, то
-                long delta = millis() - lastBeat;       //  находим дельту по времени между ударами
-                lastBeat = millis();                    //  Обновляем счётчик
-                beatsPerMinute = 60 / (delta / 1000.0); //  Вычисляем количество ударов в минуту
-                if (beatsPerMinute < 255 && beatsPerMinute > 20)
-                {                                             //  Если количество ударов в минуту находится в промежутке между 20 и 255, то
-                    rates[rateSpot++] = (byte)beatsPerMinute; //  записываем это значение в массив значений ЧСС
-                    rateSpot %= 10;                           //  Задаём порядковый номер значения в массиве, возвращая остаток от деления и присваивая его переменной rateSpot
-                    beatAvg = 0;                              //  Обнуляем переменную и
-                    for (byte x = 0; x < 10; x++)
-                    {                        //  в цикле выполняем усреднение значений (чем больше RATE_SIZE, тем сильнее усреднение)
-                        beatAvg += rates[x]; //  путём сложения всех элементов массива
-                    }
-                    beatAvg /= 10; //  а затем деления всей суммы на коэффициент усреднения (на общее количество элементов в массиве)
-                                   // Serial.print("Внутри дополнительного потока:   ");
-                                   // Serial.println(beatAvg);
-                }
+                                // Take average of readings
+                                beatAvg = 0;
+                                for (byte x = 0; x < 6; x++)
+                                    beatAvg += rates[x];
+                                beatAvg /= 6;
+                            }
+                        }
+            */
+            if (ir < 50000)
+            {
+                sinus = 0;
+                BPM = 0;
             }
+            else
+            {
+                sinus = ir;
+                BPM = beatAvg;
+            }
+            Serial.println(sinus);
+
+            uint8_t buffer[9] = {
+                0,
+            };
+            memcpy(&buffer[1], (uint8_t *)&sinus, 4);
+            memcpy(&buffer[5], (uint8_t *)&BPM, 4);
+            this->NotifyCharacteristic->writeValue(buffer, sizeof(buffer));
         }
     }
 
     void pre_loop()
     {
-        max30105 = new MAX30105();
-
         doWork = true;
+        runThread = new Thread();
+        runThread->start(callback(this, &MAX30102::execute));
 
-        if (!max30105->begin())
-        {                         //  Инициируем работу с модулем. Если инициализация не прошла, то
-            ErrorMAX30102 = true; //  выводим сообщение об этом в монитор последовательного порта
-        }
-        else
+        Serial.begin(9600);
+        particleSensor.begin(Wire, I2C_SPEED_FAST);
+        byte ledBrightness = 0x77;
+        byte sampleAverage = 4;
+        byte ledMode = 3;
+        int sampleRate = 1000;
+        int pulseWidth = 411;
+        int adcRange = 16384;
+        particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange);
+        particleSensor.setPulseAmplitudeRed(0);
+        particleSensor.setPulseAmplitudeGreen(0);
+
+        unblockedValue = 0;
+        for (byte x = 0; x < 32; x++)
         {
-            ErrorMAX30102 = false;
-
-            max30105->setup();                    //  Устанавливаем настройки для сенсора по умолчанию
-            max30105->setPulseAmplitudeRed(0x0A); //  Выключаем КРАСНЫЙ светодиод для того, чтобы модуль начал работу
-            max30105->setPulseAmplitudeGreen(0);  //  Выключаем ЗЕЛЁНЫЙ светодиод
-
-            runThread = new Thread();
-            runThread->start(callback(this, &MAX30102::execute));
+            unblockedValue += particleSensor.getIR();
         }
+        unblockedValue /= 32;
     }
     void post_loop()
     {
@@ -98,40 +117,5 @@ private:
     }
     void loop()
     {
-
-        float BPM;
-
-        // Serial.print("  Отдает датчик: ");
-        // Serial.print(beatAvg);
-        // Serial.print("  неусредненное значение: ");
-        // Serial.println(beatsPerMinute);
-
-        if (irValue < 50000)
-        {
-            BPM = 0;
-            // Serial.println("Ошибка   irValue < 50000");
-        }
-        else
-        {
-            BPM = beatAvg;
-            // Serial.print("Условие else");
-            // Serial.print("  Переменная: ");
-            // Serial.println(BPM);
-        }
-
-        uint8_t buffer[5] = {
-            0,
-        };
-
-        if (ErrorMAX30102 == true)
-        {
-            buffer[0] = 1;
-        }
-        else
-        {
-
-            memcpy(&buffer[1], (uint8_t *)&BPM, sizeof(BPM));
-        }
-        this->NotifyCharacteristic->writeValue(buffer, sizeof(buffer));
     }
 };
