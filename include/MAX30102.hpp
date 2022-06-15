e4b900097348c1440a1b8e50aa7d99d9af69388b
leo4812 <leo4812@icloud.com>
2022-06-15 16:24:34 +0300

    NewMAX30102

--- include/MAX30102.hpp
+++ include/MAX30102.hpp
@@ -13,12 +13,11 @@ class MAX30102 : public BaseSensor
 public:
     MAX30105 particleSensor;
 
-    long unblockedValue;
-    long lastBeat = 0;
-    float beatsPerMinute;
-    int beatAvg;
-    // const byte RATE_SIZE = 6;    // Increase this for more averaging. 4 is good.
-    byte rates[6]; // Array of heart rates
+    volatile long unblockedValue;
+    volatile long lastBeat = 0;
+    volatile float beatsPerMinute;
+    volatile int beatAvg;
+    byte rates[100]; // Array of heart rates
     byte rateSpot = 0;
 
     volatile uint32_t ir = 0;
@@ -26,8 +25,9 @@ public:
     volatile uint32_t sinus = 0; // Переменная синусойды
     volatile uint8_t BPM = 0;    // Переменная усредненного количества ЧСС
 
+    bool ErrorMAX30102 = false;
+
     Thread *runThread;
-    Thread *bpmThread;
 
     bool doWork = true;
 
@@ -44,27 +44,6 @@ private:
     {
         while (doWork)
         {
-
-            /*
-                        if (checkForBeat(ir) == true)
-                        {
-                            long delta = millis() - lastBeat;
-                            lastBeat = millis();
-                            beatsPerMinute = 60 / (delta / 1000.0);
-                            if (beatsPerMinute < 255 && beatsPerMinute > 20)
-                            {
-                                rates[rateSpot++] = (byte)beatsPerMinute; // Store this reading in the array
-                                rateSpot %= 6;                    // Wrap variable
-
-                                // Take average of readings
-                                beatAvg = 0;
-                                for (byte x = 0; x < 6; x++)
-                                    beatAvg += rates[x];
-                                beatAvg /= 6;
-                            }
-                        }
-            */
-
             uint32_t ValueArray[5] = {
                 0,
             };
@@ -74,33 +53,42 @@ private:
                 if (ir < 50000)
                 {
                     sinus = 0;
-                    // BPM = 0;
                 }
                 else
                 {
                     sinus = ir;
-                    // BPM = beatAvg;
                 }
                 ValueArray[i] = sinus;
+            }
+            uint8_t OutBPM = 0;
+
+            if (ir < 50000)
+            {
+                OutBPM = 0;
+            }
+            else
+            {
+                OutBPM = BPM;
             }
 
             uint8_t buffer[22] = {
                 0,
             };
-            memcpy(&buffer[2], (uint8_t *)&ValueArray[0], 4);
-            memcpy(&buffer[6], (uint8_t *)&ValueArray[1], 4);
-            memcpy(&buffer[10], (uint8_t *)&ValueArray[2], 4);
-            memcpy(&buffer[14], (uint8_t *)&ValueArray[3], 4);
-            memcpy(&buffer[18], (uint8_t *)&ValueArray[4], 4);
+            if (ErrorMAX30102 == true)
+            {
+                buffer[0] = 1;
+            }
+            else
+            {
+                buffer[0] = 0;
+                memcpy(&buffer[1], (uint8_t *)&OutBPM, 1);
+                memcpy(&buffer[2], (uint8_t *)&ValueArray[0], 4);
+                memcpy(&buffer[6], (uint8_t *)&ValueArray[1], 4);
+                memcpy(&buffer[10], (uint8_t *)&ValueArray[2], 4);
+                memcpy(&buffer[14], (uint8_t *)&ValueArray[3], 4);
+                memcpy(&buffer[18], (uint8_t *)&ValueArray[4], 4);
+            }
             this->NotifyCharacteristic->writeValue(buffer, sizeof(buffer));
-        }
-    }
-
-    void execute_bpm()
-    {
-        while (doWork)
-        {
-            // БЛА БЛА БЛА поток
         }
     }
 
@@ -110,27 +98,32 @@ private:
         runThread = new Thread();
         runThread->start(callback(this, &MAX30102::execute));
 
-        bpmThread = new Thread();
-        bpmThread->start(callback(this, &MAX30102::execute_bpm));
-
         Serial.begin(9600);
-        particleSensor.begin(Wire, I2C_SPEED_FAST);
-        byte ledBrightness = 0x77;
-        byte sampleAverage = 4;
-        byte ledMode = 3;
-        int sampleRate = 1000;
-        int pulseWidth = 411;
-        int adcRange = 16384;
-        particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange);
-        particleSensor.setPulseAmplitudeRed(0);
-        particleSensor.setPulseAmplitudeGreen(0);
-
-        unblockedValue = 0;
-        for (byte x = 0; x < 32; x++)
+        if (!particleSensor.begin(Wire, I2C_SPEED_FAST))
         {
-            unblockedValue += particleSensor.getIR();
+            ErrorMAX30102 = true;
         }
-        unblockedValue /= 32;
+        else
+        {
+            ErrorMAX30102 = false;
+
+            byte ledBrightness = 0x77;
+            byte sampleAverage = 4;
+            byte ledMode = 3;
+            int sampleRate = 1000;
+            int pulseWidth = 411;
+            int adcRange = 16384;
+            particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange);
+            particleSensor.setPulseAmplitudeRed(0);
+            particleSensor.setPulseAmplitudeGreen(0);
+
+            unblockedValue = 0;
+            for (byte x = 0; x < 32; x++)
+            {
+                unblockedValue += particleSensor.getIR();
+            }
+            unblockedValue /= 32;
+        }
     }
     void post_loop()
     {
@@ -139,12 +132,27 @@ private:
         {
             runThread = nullptr;
         }
-        if (bpmThread != nullptr)
-        {
-            bpmThread = nullptr;
-        }
+        ErrorMAX30102 = false;
     }
     void loop()
     {
+        if (checkForBeat(ir) == true)
+        {
+            long delta = millis() - lastBeat;
+            lastBeat = millis();
+            beatsPerMinute = 60 / (delta / 1000.0);
+            if (beatsPerMinute < 255 && beatsPerMinute > 20)
+            {
+                rates[rateSpot++] = (byte)beatsPerMinute; // Store this reading in the array
+                rateSpot %= 100;                          // Wrap variable
+
+                // Take average of readings
+                beatAvg = 0;
+                for (byte x = 0; x < 100; x++)
+                    beatAvg += rates[x];
+                beatAvg /= 100;
+            }
+        }
+        BPM = beatAvg;
     }
 };
